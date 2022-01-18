1c62d238aeb9ab3d91a52efe322910f60078edae
oichan <makoto.uwaya@gmail.com>
2022-01-18 21:15:00 +0900

    🔧 Change baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Makoto Uwaya Portfolio',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/portfolio/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
