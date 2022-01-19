e500dc2be49cc08f96c40c3b9495d2cbd2a1ebe9
oichan <makoto.uwaya@gmail.com>
2022-01-19 12:55:38 +0900

    🔊 Add gtag plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,6 +31,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-WQFQWCBZT4',
+          anonymizeIP: false
+        }
       }),
     ],
   ],
